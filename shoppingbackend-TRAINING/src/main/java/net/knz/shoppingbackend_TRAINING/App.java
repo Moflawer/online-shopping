7057a5daed2bee4453b5d36e18c0daba7eefd6bc
Mehdi BOUDOUR <geronimotoutcourt@gmail.com>
2018-04-13 21:33:06 +0200

    My Project Setup Completed Successfully

---
+++ shoppingbackend-TRAINING/src/main/java/net/knz/shoppingbackend_TRAINING/App.java
@@ -0,0 +1,13 @@
+package net.knz.shoppingbackend_TRAINING;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
